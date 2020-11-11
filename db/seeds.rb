07d07d53305b367d00144a186ae4277fae47d446
shane <shanecolbyzero8@gmail.com>
2020-11-10 18:28:20 -0600

    added songs to app via db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+song = Song.new(title: "merry christmas", album: "christmas is here", artist: "2-pac", year: 1994)
+song.save
+
+song = Song.new(title: "happy new year", album: "new year", artist: "cisco", year: 2002)
+song.save
+
+song = Song.new(title: "basketball", album: "sports songs", artist: "Allen Iverson", year: 2007)
+song.save
